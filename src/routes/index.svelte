116bd4b4fc42860a6092d31d5fcbe2785cfd8faa
raymondboswel <raymondboswel@gmail.com>
2021-01-29 19:47:29 +0200

    Added svelte-material-ui

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -1,5 +1,7 @@
 <script>
 	import successkid from 'images/successkid.jpg';
+	
+  import Button, {Label} from '@smui/button';
 </script>
 
 <style>
@@ -47,4 +49,6 @@
 	<figcaption>Have fun with Sapper!</figcaption>
 </figure>
 
-<p><strong>Try editing this file (src/routes/index.svelte) to test live reloading.</strong></p>
+<p><strong>Try editing this file (src/routes/index.svelte) to test live reloading.</strong>
+	<Button><Label>Material Button</Label></Button>
+</p>
